b3d4aba4f9b8d36d5ce39a5ddf49d75a5dbf8b2f
RafaelWernesbach <126472411+RafaelWernesbach@users.noreply.github.com>
2023-05-11 10:48:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function consultar() {
+    var matricula = parseInt(prompt('Insira sua matricula'));
+    var nome = prompt('Insira o Nome');
+    var notaP1 = parseFloat(prompt('Insira a nota da P1'));
+    var notaP2 = parseFloat(prompt('Insira a nota da P2'));
+    var media = (notaP1 + notaP2) / 2;
+
+    console.log(media);
+}
+
+
+
+
+
+
